61e586c403ba28fedb2e05922a57cd2f46ec3892
Miguel Vargas <mavargas101@gmail.com>
2022-01-29 18:06:27 +0000

    add button styles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let currentDisplay = "";
 let previousDisplay = "";
 let currentOperator = "";
 
+const maxCharacters = 8;
 
 // EVENT LISTENERS
 const numbers = document.querySelectorAll(".numberBtn");
@@ -13,17 +14,18 @@ const operations = document.querySelectorAll(".operationBtn");
 for(let i = 0; i < operations.length; i++){
     operations[i].addEventListener('click', function(){
         if(currentDisplay != ""){
-            if(currentOperator != "" || currentOperator === operations[i].innerText){calculate(previousDisplay, currentDisplay)}
+            if(currentOperator != ""){calculate(previousDisplay, currentDisplay)}
             currentOperator = operations[i].innerText;
             previousDisplay = `${currentDisplay}`;
             updateDisplay();
             currentDisplay = "";
-        }
-        else{
+            console.log(`Ran thru and ${previousDisplay}`);
+        }else{
             currentOperator = operations[i].innerText;
-            previousDisplay = `${previousDisplay}`;
+            currentDisplay = previousDisplay;
+            updateDisplay();
+            currentDisplay = "";
         }
-
     })
 }
 
@@ -36,11 +38,6 @@ const prevDisplay = document.querySelector('.prevDisplay');
 const allClearBtn = document.querySelector('.allClearBtn');
 allClearBtn.addEventListener('click', () => allClear());
 
-function operationBtn(btn){
-    currentOperator = btn.innerText;
-
-}
-
 function allClear(){
     currentDisplay = "0";
     previousDisplay = "";
@@ -49,9 +46,8 @@ function allClear(){
 }
 function appendNumber(button){
     if(currentDisplay == 0){currentDisplay = button.innerText}
-    else{currentDisplay += button.innerText;}
+    else if(currentDisplay.length <= maxCharacters){currentDisplay += button.innerText;}
     updateDisplay();
-    
 }
 
 function add(num1, num2){
@@ -68,30 +64,31 @@ function divide(num1, num2){
 }
 function updateDisplay(){
     display.innerText = currentDisplay;
-    prevDisplay.innerText = previousDisplay;
+    prevDisplay.innerText = `${previousDisplay} ${currentOperator}`;
 }
 
 function calculate(num1, num2){
+    if(currentDisplay != ""){
     switch (currentOperator){
             case '+':
-                previousDisplay += ` ${num2}`;
+                previousDisplay += ` ${currentOperator} ${num2} =`;
                 currentDisplay = add(parseInt(num1), parseInt(num2));
                 break;
             case '-':
-                previousDisplay += ` ${num2}`;
+                previousDisplay += ` ${currentOperator} ${num2} =`;
                 currentDisplay = subtract(parseInt(num1), parseInt(num2));
                 break;
             case '*':
-                previousDisplay += ` ${num2}`;
+                previousDisplay += ` ${currentOperator} ${num2} =`;
                 currentDisplay = multiply(parseInt(num1), parseInt(num2));
                 break;
             case '/':
-                previousDisplay += ` ${num2}`;
+                previousDisplay += ` ${currentOperator} ${num2} =`;
                 currentDisplay = divide(parseInt(num1), parseInt(num2));
                 break;
         } 
         currentOperator = '';
     
     updateDisplay();
-
+    }
 }
